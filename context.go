caeret/zag

Rename package.

context.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-package routing
+package zag
 
 import (
 	"net/http"

reader.go
@@ -1,4 +1,4 @@
-package routing
+package zag
 
 import (
 	"encoding"

router_test.go
@@ -1,12 +1,13 @@
-package routing
+package zag
 
 import (
 	"errors"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRouterNotFound(t *testing.T) {

caeret/mat

context.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-package routing
+package mat
 
 import (
 	"net/http"

reader.go
@@ -1,4 +1,4 @@
-package routing
+package mat
 
 import (
 	"encoding"

router_test.go
@@ -1,12 +1,13 @@
-package routing
+package mat
 
 import (
 	"errors"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRouterNotFound(t *testing.T) {