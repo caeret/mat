caeret/zag

Add catch all method.

catchall.go
@@ -1,0 +1,6 @@
+package zag
+
+func (rg *RouteGroup) CatchAll(handlers ...Handler) *RouteGroup {
+	rg.router.catchAll.Insert(rg.prefix, handlers)
+	return rg
+}

caeret/mat

catchall.go
@@ -1,0 +1,6 @@
+package mat
+
+func (rg *RouteGroup) CatchAll(handlers ...Handler) *RouteGroup {
+	rg.router.catchAll.Insert(rg.prefix, handlers)
+	return rg
+}